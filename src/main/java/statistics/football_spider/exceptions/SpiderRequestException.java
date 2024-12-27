1bf5dfa2e0eb313acc8a0a58427a968ec4540ed5
kantror <148765266+kantror@users.noreply.github.com>
2024-12-27 11:34:36 +0100

    Create SpiderRequestException.java

---
+++ src/main/java/statistics/football_spider/exceptions/SpiderRequestException.java
@@ -0,0 +1,20 @@
+package statistics.football_spider.exceptions;
+
+public class SpiderRequestException extends Exception {
+
+	private static final long serialVersionUID = 6189374396857825180L;
+
+	public SpiderRequestException() {
+		
+		super();
+		
+	}
+	
+	public SpiderRequestException(String message) {
+		
+		super(message);
+		
+	}
+	
+	
+}
